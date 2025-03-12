da667d2e5b29400e13ecd01a9129e02d7d4c494e
mrhpn <mr.htetphyonaing@gmail.com>
2025-03-12 20:10:52 +0630

    feat: added happening now section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LandingSection from '../components/LandingSection';
 import TrendingSection from '../components/TrendingSection';
+import HappeningNowSection from '../components/HappeningNow';
 
 const Home: React.FC = () => {
   return (
     <div>
       <LandingSection />
       <TrendingSection />
+      <HappeningNowSection />
     </div>
   );
 };
